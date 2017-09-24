f94f58e0329ace7b92e0dc7acdb09f79508a2630
Kris Poedjosoedarmo <neorou@gmail.com>
2017-09-24 09:37:54 -0400

    added new document insertion

--- routes/documents.js
+++ routes/documents.js
@@ -2,6 +2,22 @@ var express = require('express');
 // var bodyParser = require('body-parser');
 var router = express.Router();
 
+// load docwd configuration file, check for config.json first, if not use config.default.json
+var docwdConfig = require('../config.default.json');
+var pg = require('pg');
+
+// console.log(docwdConfig['database']['host']); // test that this is read properly
+const { Pool,  Client } = require('pg');
+
+const dbuser = docwdConfig['database']['user'];
+const dbpassword = docwdConfig['database']['password'];
+const dbhost = docwdConfig['database']['host'];
+const dbport = docwdConfig['database']['port'];
+const dbname = 'docwd';
+
+// const connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
+const connectionString = 'postgresql://' + dbuser + ':' + dbpassword + '@' + dbhost + ':' + dbport + '/' + dbname;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a document resource');
@@ -16,6 +32,23 @@ router.get('/new', function(req, res, next) {
   res.send(htmlData);
 });
 
+// process form above
+router.post('/new', function(req, res, next) {
+  const pgclient = new Client({
+    connectionString: connectionString,
+  });
+  pgclient.connect();
+  var sqlQuery = 'insert into documentmd (ownerid, mddata, title) values ($1,$2,$3)';
+  var bodyParams = req.body;
+  pgclient.query(sqlQuery, [1,bodyParams.documentData,bodyParams.documentTitle], (err, result) => {
+    // console.log(err, res)
+    pgclient.end();
+    res.send(JSON.stringify(result));
+  });
+
+});
+
+/*
 router.post('/new', function(req, res, next) {
   // console.log(req.body);
   var returnData = req.body;
@@ -23,5 +56,6 @@ router.post('/new', function(req, res, next) {
   returnData['query'] = query;
   res.json(returnData);
 });
+*/
 
 module.exports = router;
